dd7dacc17744ea6281907b1f2f1ef2c96e9d7f15
hjiannan <r60r60w@gmail.com>
2014-12-04 14:36:22 -0500

    12/4
    Fix the bugs in isDependency.

---
+++ ParseInstr.h
@@ -0,0 +1,204 @@
+//
+//  ParseInstr.h
+//  Dependency
+//
+//  Created by Jiannan Huang on 12/1/14.
+//  Copyright (c) 2014 Jiannan Huang. All rights reserved.
+//
+
+#ifndef __Dependency__ParseInstr__
+#define __Dependency__ParseInstr__
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+struct Instr {
+    Instr(): operation(""), reg1(""), reg2("") {}
+    string operation;
+    string reg1;
+    string reg2;
+    string reg3;
+    int rep;
+};
+
+
+
+
+void inputOne_1(Instr& instr, vector<Instr>& instrVec){ /* e.g. op r1, waste */
+    string waste;
+    cin >> instr.reg1;
+    cin >> waste;
+    instrVec.push_back(instr);
+    
+}
+
+///////////////////
+
+
+
+void inputTwo_1(Instr& instr, vector<Instr>& instrVec){ /* e.g. op r1, r2 */
+    string waste;
+    cin >> instr.reg1;
+    cin >> instr.reg2;
+    instrVec.push_back(instr);
+}
+
+
+void inputTwo_2(Instr& instr, vector<Instr>& instrVec){ /* e.g. op r1, r2, waste */
+    string waste;
+    cin >> instr.reg1;
+    cin >> instr.reg2;
+    cin >> waste;
+    instrVec.push_back(instr);
+}
+
+void inputTwo_3(Instr& instr, vector<Instr>& instrVec){ /* e.g. op r1, [r2, waste] */
+    string waste;
+    cin >> instr.reg1;
+    cin >> instr.reg2;
+    cin >> waste;
+    instr.reg2 = instr.reg2.substr(1, 10);
+    instrVec.push_back(instr);
+}
+
+
+void inputTwo_4(Instr& instr, vector<Instr>& instrVec){ /* e.g. op r1, r2, waste waste */
+    string waste;
+    cin >> instr.reg1;
+    cin >> instr.reg2;
+    cin >> waste;
+    cin >> waste;
+    instrVec.push_back(instr);
+}
+
+///////////////////
+
+void inputThree_1(Instr& instr, vector<Instr>& instrVec){ /* e.g. op r1, r2, r3 */
+    string waste;
+    cin >> instr.reg1;
+    cin >> instr.reg2;
+    cin >> instr.reg3;
+    instrVec.push_back(instr);
+}
+
+
+
+
+void parseInstr(vector<Instr>& instrVec){
+    Instr instr;
+    string waste;
+    while (cin >> instr.operation) {
+        if (instr.operation.substr(0, 3) == "ldr" || instr.operation.substr(0,3) == "str") {
+            /* op r1, [r2, waste] */
+            inputTwo_3(instr, instrVec);
+            continue;
+        }
+        else if (instr.operation.substr(0, 3) == "rsb"){
+            cin >> instr.reg1 >> instr.reg2;
+            string temp;
+            cin >> temp;
+            if (temp[0] == '#') {
+                instrVec.push_back(instr);
+                continue;
+            }
+            instr.reg3 = temp;
+            instrVec.push_back(instr);
+            continue;
+        }
+        else if( instr.operation.substr(0, 3) == "tst" || instr.operation.substr(0, 3) == "cmps"){
+            cin >> instr.reg1;
+            string temp;
+            cin >> temp;
+            if (temp[0] == '#') {
+                instrVec.push_back(instr);
+                continue;
+            }
+            instr.reg2 = temp;
+            instrVec.push_back(instr);
+            continue;
+        }
+        else if(instr.operation.substr(0, 3) == "mov" ){
+            /* mov type instr */
+            if (instr.operation == "mov" ){ /* op r1, r2 */
+                inputTwo_1(instr, instrVec);
+                continue;
+            }
+            if (instr.operation == "movt.w") {
+                inputTwo_2(instr, instrVec);
+                continue;
+            }
+            if (instr.operation == "movs") {
+                cin >> instr.reg1;
+                string temp;
+                cin >> temp;
+                if (temp[0] == '#') {
+                    instrVec.push_back(instr);
+                    continue;
+                }
+                instr.reg2 = temp;
+                getline(cin, waste);
+                if (instr.reg2[instr.reg2.size()-1] == ',') instr.reg2.pop_back();
+                instrVec.push_back(instr);
+                continue;
+            }
+            
+            if (instr.operation == "mov.w") {
+                inputOne_1(instr, instrVec);
+                continue;
+            }
+        }
+        else if(instr.operation.substr(0, 3) == "add" || instr.operation.substr(0, 3) == "sub" || instr.operation.substr(0, 3) == "orr"){
+            if (instr.operation == "add" || instr.operation.substr(0, 8) == "addi_uop"
+                || instr.operation == "sub" || instr.operation.substr(0, 8) == "subi_uop") {
+                inputTwo_2(instr, instrVec);
+                continue;
+            }
+            if (instr.operation[3] == 's'){
+                cin >> instr.reg1 >> instr.reg2;
+                string temp;
+                cin >> temp;
+                if (temp[0] == '#') {
+                    instrVec.push_back(instr);
+                    continue;
+                }
+                instr.reg3 = temp;
+                instrVec.push_back(instr);
+                continue;
+            }
+            if (instr.operation.substr(3, 2) == ".w") {
+                cin >> instr.reg1 >> instr.reg2;
+                string temp;
+                cin >> temp;
+                if (temp[0] == '#') {
+                    instrVec.push_back(instr);
+                    continue;
+                }
+                instr.reg3 = temp;
+                getline(cin, waste);
+                instrVec.push_back(instr);
+                continue;
+                
+            }
+        }
+        
+    }
+    for (auto& inst : instrVec){
+     //   cout << inst.reg1[inst.reg1.size()-1] << " " << inst.reg2 << " " << inst.reg3 << endl;
+        if (inst.reg1[inst.reg1.size()-1] == ',') {
+            inst.reg1.pop_back();
+        }
+
+        if (inst.reg2[inst.reg2.size()-1] == ',') {
+            inst.reg2.pop_back();
+        }
+
+        if (inst.reg3[inst.reg3.size()-1] == ',') {
+            inst.reg3.pop_back();
+        }
+    }
+}
+
+#endif /* defined(__Dependency__ParseInstr__) */
---
+++ main.cpp
@@ -0,0 +1,94 @@
+//
+//  main.cpp
+//  Dependency
+//
+//  Created by Jiannan Huang on 11/30/14.
+//  Copyright (c) 2014 Jiannan Huang. All rights reserved.
+//
+
+#include "ParseInstr.h"
+#include <unordered_map>
+
+
+void checkDependency(vector<Instr>& instrVec);
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    ifstream arq(getenv("MYARQ"));    cin.rdbuf(arq.rdbuf());
+    vector<Instr> instrVec;
+    parseInstr(instrVec);
+    
+    for(unsigned i = 0; i < instrVec.size(); i++){
+        instrVec[i].rep = i;
+        cout << instrVec[i].rep << setw(10) << instrVec[i].operation << setw(7) << instrVec[i].reg1 << setw(7) << instrVec[i].reg2 << setw(7) << instrVec[i].reg3 << endl;
+    }
+    cout << endl;
+    
+    checkDependency(instrVec);
+    for(unsigned i = 0; i < instrVec.size(); i++){
+        instrVec[i].rep = i;
+        cout << instrVec[i].rep << setw(10) << instrVec[i].operation << setw(7) << instrVec[i].reg1 << setw(7) << instrVec[i].reg2 << setw(7) << instrVec[i].reg3 << endl;
+    }
+    return 0;
+}
+
+
+bool isDependent(Instr& instr1, Instr& instr2){
+    if (instr1.reg1 != "" && instr1.reg1 == instr2.reg1) {
+        return true;
+    }
+    if ( instr1.reg2 != "" && instr1.reg2 == instr2.reg2) {
+        return true;
+    }
+    if (instr1.reg3 != "" && instr1.reg3 == instr2.reg3 ) {
+        return true;
+    }
+    return false;
+}
+
+
+int Find(int id, vector<Instr>& instrVec){
+    if (id != instrVec[id].rep) {
+        instrVec[id].rep = Find(instrVec[id].rep, instrVec);
+    }
+    return instrVec[id].rep;
+}
+
+
+void outputResult(unordered_map<int, Instr> independent_instr){
+    cout << "The number of independent instructions is " << independent_instr.size() << ". They are:\n";
+    for (unsigned i = 0; i < independent_instr.size(); ++i){
+        cout << setw(10) << independent_instr[i].operation << setw(7) <<
+        independent_instr[i].reg1 << setw(7) << independent_instr[i].reg2
+        << setw(7) << independent_instr[i].reg3 << endl;
+    }
+}
+
+void checkDependency(vector<Instr>& instrVec){
+
+    for (unsigned i = 0; i < instrVec.size(); i++) {
+        for(unsigned j = i+1; j < instrVec.size(); j++){
+            if(isDependent(instrVec[i], instrVec[j])){
+                instrVec[j].rep = i;
+            }
+        }
+    }
+    unordered_map<int, Instr> independent_instr;
+    for (unsigned i = 0; i < instrVec.size(); i++){
+        independent_instr[Find(i, instrVec)] = instrVec[Find(i, instrVec)];
+    }
+    
+    /**  output independent instructons stored in independent_instr
+     */
+    outputResult(independent_instr);
+    
+}
+
+
+
+
+
+
+
+
+
